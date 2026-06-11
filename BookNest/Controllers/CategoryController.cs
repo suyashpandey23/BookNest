a0c5771ce7c84565a64d57f3f1164bc0eab1a5da
suyashpandey23 <suyashpandey145@gmail.com>
2026-06-11 09:00:32 +0530

    update, delete, notification functionality in category section

--- BookNest/Controllers/CategoryController.cs
+++ BookNest/Controllers/CategoryController.cs
@@ -6,11 +6,102 @@ namespace BookNest.Controllers;
 public class CategoryController : Controller {
     // GET
     private readonly ApplicationDbContext _context;
+
     public CategoryController(ApplicationDbContext db) {
         _context = db;
     }
+
     public IActionResult Index() {
         var categories = _context.Categories.ToList();
         return View(categories);
     }
+
+    public IActionResult Create() {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [ActionName("Create")]
+    public IActionResult CreatePOST(Category category) {
+
+        //match if same name is already present
+        if (!string.IsNullOrEmpty(category.Name) &&
+            _context.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower())) {
+            ModelState.AddModelError("Name", "Category name already exists");
+        }
+
+        if (ModelState.IsValid) {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+
+    }
+
+    public IActionResult Update(int? id) {
+
+        if (id == null || id == 0) {
+            return NotFound();
+        }
+
+        var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+        if (category == null) {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [ActionName("Update")]
+    public IActionResult UpdatePOST(Category category) {
+        if( !string.IsNullOrEmpty(category.Name) && _context.Categories.Any(c=>c.Name.ToLower()==category.Name.ToLower() && c.Id != category.Id))
+        {
+            ModelState.AddModelError("Name", "Category name already exists");
+        }
+        if (ModelState.IsValid) {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+    
+    
+    public IActionResult Delete(int? id) {
+
+        if (id == null || id == 0) {
+            return NotFound();
+        }
+
+        var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+        if (category == null) {
+            return NotFound();
+        }
+        
+        return View(category);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [ActionName("Delete")]
+    public IActionResult DeletePOST(int? id) {
+       var category=_context.Categories.FirstOrDefault(c=>c.Id==id);
+       if (category == null) {
+           return NotFound();
+       }
+       _context.Categories.Remove(category);
+       _context.SaveChanges();
+       TempData["success"] = "Category deleted successfully";
+       return RedirectToAction("Index");
+       
+    }
+    
 }
--- BookNest/Models/Category.cs
+++ BookNest/Models/Category.cs
@@ -4,7 +4,10 @@ namespace BookNest.Models;
 public class Category {
     //[Key]
     public int Id{ get; set; }
+    [Required]
+    [StringLength(100)]
     public string Name{ get; set; } = string.Empty;
     
+    [Range(0,100,ErrorMessage = "Range must be between 0 and 100")]
     public int DisplayOrder{ get; set; }
 }
